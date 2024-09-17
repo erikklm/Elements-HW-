c99d95881558cb0632c18e18c659454eea0e1429
erikklem <erikklem@iu.edu>
2024-09-17 15:28:01 -0400

    Done

--- Homework/HW1/Assignment 1.py
+++ Homework/HW1/Assignment 1.py
@@ -1,105 +1,72 @@
+#!/usr/local/bin/python3
+#
+# mystical_castle.py : a maze solver
+#
+# Submitted by : Selina Zheng (selzheng), Xue Xiao (xuexiao), Erik Klem (erikklem)
+#
+# Based on skeleton code provided in CSCI B551, Fall 2024.
 
-class FindWayOut():
+import sys
+from collections import deque
+# Parse the map from a given filename
+def parse_map(filename):
+        with open(filename, "r") as f:
+                return [[char for char in line] for line in f.read().rstrip("\n").split("\n")][3:]
+                
+# Check if a row,col index pair is on the map
+def valid_index(pos, n, m):
+        return 0 <= pos[0] < n  and 0 <= pos[1] < m
 
-    myDic = {'U':[-1,0], 'D':[1,0], 'R':[0,1], 'L':[0,-1]}
+# Find the possible moves from position (row, col)
+def moves(map, row, col):
+        moves=((row+1,col), (row-1,col), (row,col-1), (row,col+1))
 
-    def __init__(self, file) -> None:
-        self.board = self.makeBoard(file)
-        self.start = self.startOfBoard(self.board)
-        self.visited = []
-    
-    def makeBoard(self, file): #This function is going to help actually read in the code and put it into a board variable that is an instance variable
-        board = []
-        with open(file, 'r') as f:
-            for line in f:
-                board.append(list(line.strip()))
-        return board
-    
-    def startOfBoard(self, board):
-        start = []
-        for indexX,x in enumerate(board):
-            for indexY,y in enumerate(x):
-                if y == 'p':
-                    start = [indexX, indexY]
-                    return start
-    
-    def movingOnBoard(self, x, y, path, dist):
-        if self.board[x][y] == '@':
-            return dist, path
+        # Return only moves that are within the castle_map and legal (i.e. go through open space ".")
+        return [ move for move in moves if valid_index(move, len(map), len(map[0])) and (map[move[0]][move[1]] in ".@" ) ]
 
+# Perform search on the map
+#
+# This function MUST take a single parameter as input -- the castle map --
+# and return a tuple of the form (move_count, move_string), where:
+# - move_count is the number of moves required to navigate from start to finish, or -1
+#    if no such route exists
+# - move_string is a string indicating the path, consisting of U, L, R, and D characters
+#    (for up, left, right, and down)
 
-        keepTrack_distance = float('inf')
-        keepTrack_path = None
+def search(castle_map):
+        # Find current start position
+        current_loc=[(row_i,col_i) for col_i in range(len(castle_map[0])) for row_i in range(len(castle_map)) if castle_map[row_i][col_i]=="p"][0]
+        fringe = deque([(current_loc, 0, "")]) # Initialize fringe as a deque
+        visited = set() # Track visited nodes
 
-        for walk, [dirX, dirY] in self.myDic.items():
-            tempX = dirX + x
-            tempY = dirY + y
+        while fringe:
+                (curr_move, curr_dist, path)=fringe.popleft() # Dequeue
+                visited.add(curr_move)
+                
+                for move in moves(castle_map, *curr_move):
+                        if move not in visited:
+                                if castle_map[move[0]][move[1]]=="@":
+                                        return (curr_dist + 1, path + direction(curr_move, move))  # Reach the goal
+                                else:
+                                        fringe.append((move, curr_dist + 1, path + direction(curr_move, move))) # Enqueue the new move
+        return (-1, "") # No solution
 
-            if(self.checkIfWall(tempX, tempY) and (self.checkIfMove(tempX, tempY))):
-                self.visited.append([tempX, tempY])
-
-                #path.append(walk)
-                check_keepTrack_distance, check_keepTrack_path = self.movingOnBoard(tempX, tempY, path + [walk], dist +1)
-
-                if (check_keepTrack_distance < keepTrack_distance):
-                    keepTrack_distance = check_keepTrack_distance
-                    keepTrack_path = check_keepTrack_path
-
-                self.visited.remove([tempX, tempY])
-
-            
-
-        return keepTrack_distance, keepTrack_path
+# Helper function to determine the direction
+def direction(from_pos, to_pos):
+    if to_pos[0] == from_pos[0] + 1:
+        return "D"
+    elif to_pos[0] == from_pos[0] - 1:
+        return "U"
+    elif to_pos[1] == from_pos[1] + 1:
+        return "R"
+    elif to_pos[1] == from_pos[1] - 1:
+        return "L"
+    return ""
         
-
-
-    def checkIfWall(self, x, y):
-        if not (0 <= x < len(self.board)):
-        #if not((x >= 0) and (x < len(self.board))):
-            return False
-        #0 <= y
-        if not (0 <= y < len(self.board[1])):
-            return False
-        return True
-    
-    def checkIfMove(self, x, y):
-        if((self.board[x][y] == 'X') or ([x,y] in self.visited)):
-            return False
-        return True
-    
-
-    def convertToString(self, path):
-        string = ''
-        for i in path:
-            string += i
-        return string
-
-
-
-    def solve(self):
-        self.visited.append([self.start[0], self.start[1]])
-        dist_traveled, path_taken = self.movingOnBoard(self.start[0], self.start[1], [], 0)
-        if dist_traveled == float('inf'):
-            print("No path found")
-            return None
-        #print(dist_traveled, path_taken)
-        finalAnswer = self.convertToString(path_taken)
-        print(finalAnswer, dist_traveled)
-
-
-
-
+# Main Function
 if __name__ == "__main__":
-    import sys
-    
-    escape_solver = FindWayOut("C:\\Users\\KLEMS\\OneDrive\\Documents\\Elements-HW-\\Homework\\HW1\\castle_map. txt")
-    escape_solver.solve()
-    
-    
-
-
-
-        
-
-
-
+        castle_map=parse_map(sys.argv[1])
+        print("Shhhh... quiet while I navigate!")
+        solution = search(castle_map)
+        print("Here's the solution I found:")
+        print(str(solution[0]) + " " + solution[1])
